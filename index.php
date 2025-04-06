791a424c0661d35a238e62218c86ec31122b4e5d
Jerome labbé <labbe.jerome@gmail.com>
2025-04-06 16:02:26 +0200

    0.0.13

--- index.php
+++ index.php
@@ -1,20 +1,41 @@
-<?php get_header(); ?>
-
-<main>
-  <!-- test version 0.0.12 -->
-
-  <h2><?php the_title(); ?></h2>
-  <div class="content">
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <title><?php wp_title( '|', true, 'right' ); ?></title>
+    <link rel="stylesheet" href="<?php echo esc_url( get_stylesheet_uri() ); ?>" type="text/css" />
+    <?php get_header(); ?>
+  </head>
+  <body>
+    <h1><?php bloginfo( 'name' ); ?></h1>
+    <h2><?php bloginfo( 'description' ); ?></h2>
+ 
+    <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+ 
+    <h3><?php the_title(); ?></h3>
+ 
+    <?php the_content(); ?>
+    <?php wp_link_pages(); ?>
+    <?php edit_post_link(); ?>
+ 
+    <?php endwhile; ?>
+ 
     <?php
-    if (have_posts()) :
-      while (have_posts()) : the_post();
-        the_content();
-      endwhile;
-    else :
-      echo '<p>Contenu non trouvé.</p>';
-    endif;
+    if ( get_next_posts_link() ) {
+      next_posts_link();
+    }
     ?>
-  </div>
-</main>
-
-<?php get_footer(); ?>
+    <?php
+    if ( get_previous_posts_link() ) {
+      previous_posts_link();
+    }
+    ?>
+ 
+    <?php else: ?>
+ 
+    <p>Aucun article trouvé !</p>
+ 
+    <?php endif; ?>
+    <?php wp_footer(); ?>
+  </body>
+</html>
